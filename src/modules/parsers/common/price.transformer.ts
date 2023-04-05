650c90ded13600b4addcb98584967b51e4961e7b
Adem Koc <koc.adem.809@gmail.com>
2023-04-05 14:00:26 +0300

    fix: amount transformer

--- src/modules/parsers/common/price.transformer.ts
+++ src/modules/parsers/common/price.transformer.ts
@@ -1,7 +1,11 @@
 import { Prisma } from "@prisma/client";
 
 export function amountTransformer(priceStr: any) {
-  const amount = parseFloat(priceStr.slice(0, -3).replace(",", "."));
+  const amount = parseFloat(
+    priceStr.slice(0, -3)
+      .replace(".", "")
+      .replace(",", "."),
+  );
   return new Prisma.Decimal(amount);
 }
 
