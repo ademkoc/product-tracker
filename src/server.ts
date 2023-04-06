8519db7a6830f18f0829732dffc83e53f2ecba56
Adem Koc <koc.adem.809@gmail.com>
2023-04-06 15:27:34 +0300

    activate jobs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import { build } from './app';
+import { registerJobs } from './modules/jobs';
 
 async function server() {
   const server = await build();
-  await server.listen({ port: 3000 });
+
+  registerJobs();
+
+  server.listen({ port: 3000 }, function (err) {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  });
 }
 
 void server();
