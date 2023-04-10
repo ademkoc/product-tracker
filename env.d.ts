4cb0ca1709cef1fec3ba905bb76ed1c0c4466abb
Adem Koc <koc.adem.809@gmail.com>
2023-04-10 13:02:10 +0300

    feat: add new env variables

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NODE_ENV: string;
       DATABASE_URL: string;
       NOTIFICATION_TOPIC: string;
       NOTIFICATION_SERVICE_URL: string;
+      PORT: string;
+      SERVICE_NAME: string;
+      CHECK_PRODUCT_PRICE_IN_MINS: string;
+      PRODUCT_LAST_CHECK_IN_MINS: string;
     }
   }
 }
